4441b933fc660bc1fd25efe307b7469b41b8a211
Stephen <stephen_h369@live.co.uk>
2020-08-31 19:34:18 +0100

    added attractions and stalls arraylist to ThemePark class, can get all reviews for both

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,31 @@
+import attractions.Attraction;
+import behaviours.IReviewed;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+
+    public ThemePark(ArrayList<Attraction> attractions, ArrayList<Stall> stalls) {
+        this.attractions = attractions;
+        this.stalls = stalls;
+    }
+
+    public ArrayList<Attraction> getAttractions() {
+        return attractions;
+    }
+
+    public ArrayList<Stall> getStalls() {
+        return stalls;
+    }
+
+    public ArrayList<IReviewed> getAllReviewed() {
+        ArrayList<IReviewed> reviewed;
+        reviewed = new ArrayList<IReviewed>();
+        reviewed.addAll(attractions);
+        reviewed.addAll(stalls);
+        return reviewed;
+    }
 }
