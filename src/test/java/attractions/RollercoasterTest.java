267cff5eb03ca6232b4e96b67de597f482307e10
Stephen <stephen_h369@live.co.uk>
2020-08-31 16:46:18 +0100

    implemented ISecurity in RollerCoaster class

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,25 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor underAgeVisitor;
+    Visitor underHeightVisitor;
+    Visitor underAgeAndHeightVisitor;
+    Visitor visitor;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        underAgeVisitor = new Visitor(11, 145, 25);
+        underHeightVisitor = new Visitor(13, 135, 25);
+        underAgeAndHeightVisitor = new Visitor(10, 125, 25);
+        visitor = new Visitor(14, 150, 25);
     }
 
     @Test
@@ -27,5 +36,13 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void willNotAllowUnderAgeOrHeight() {
+        assertEquals(false, rollerCoaster.isAllowedTo(underAgeVisitor));
+        assertEquals(false, rollerCoaster.isAllowedTo(underHeightVisitor));
+        assertEquals(false, rollerCoaster.isAllowedTo(underAgeAndHeightVisitor));
+        assertEquals(true, rollerCoaster.isAllowedTo(visitor));
     }
 }
